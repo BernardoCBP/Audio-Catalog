7edcfa54fdb283548d59d611880ca47848767797
Bernardo Pineda <61992267+BernardoCBP@users.noreply.github.com>
2021-07-26 16:47:31 +0100

    Delete Source directory

--- Source/listfiles.h
+++
@@ -1,8 +0,0 @@
-#ifndef LISTFILES_H
-#define LISTFILES_H
-
-#define MAX_NAME 255 //a dimensão máxima para o nome de um ficheiro em linux é 255
-
-int listScan( char *listName, TagSet_t *data, strSet_t *sSet );
-
-#endif
